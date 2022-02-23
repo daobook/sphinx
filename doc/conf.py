6b5ed106cd8c57dc362e20e44a4173647e1eb81c
xinetzone <xinzone@outlook.com>
2022-02-23 11:54:26 +0800

    sphinxcontrib.serializinghtml

--- doc/conf.py
+++ doc/conf.py
@@ -6,7 +6,7 @@ import sphinx
 
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo',
               'sphinx.ext.autosummary', 'sphinx.ext.extlinks',
-              'sphinx.ext.intersphinx',
+              'sphinx.ext.intersphinx', 'sphinxcontrib.serializinghtml',
               'sphinx.ext.viewcode', 'sphinx.ext.inheritance_diagram']
 
 root_doc = 'contents'
